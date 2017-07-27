7335e736f16051c9cdd43cb2818755c6d3e8d935
Eric Hankinson <eric.hankinson@leandog.com>
2017-07-27 15:30:57 -0400

    Start with empty Cucumber project and gems version locked

---
+++ features/support/env.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+
+
+Before do |scenario|
+  # The +scenario+ argument is optional, but if you use it, you can get the title,
+  # description, or name (title + description) of the scenario that is about to be
+  # executed.
+
+end
+
+After do |scenario|
+  # Do something after each scenario.
+  # The +scenario+ argument is optional, but
+  # if you use it, you can inspect status with
+  # the #failed?, #passed? and #exception methods.
+
+end
+
