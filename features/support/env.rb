d6078df5d6f345a9f2ab80f152a04cc5e6b81a43
Eric Hankinson <eric.hankinson@leandog.com>
2017-08-11 15:52:13 -0400

    Added in browser management to Cucumber hooks

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,12 @@
 require 'rspec'
+require 'watir'
 
 
 Before do |scenario|
   # The +scenario+ argument is optional, but if you use it, you can get the title,
   # description, or name (title + description) of the scenario that is about to be
   # executed.
-
+  @browser = Watir::Browser.new :chrome
 end
 
 After do |scenario|
@@ -14,5 +15,9 @@ After do |scenario|
   # if you use it, you can inspect status with
   # the #failed?, #passed? and #exception methods.
 
+  if @browser
+    @browser.quit
+    @browser = nil
+  end
 end
 
